b9103ee50d3438d4a224ff453cd36695d984409c
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-01-31 22:24:39 +0100

    Create search post page

--- mopidy_jukebox/web.py
+++ mopidy_jukebox/web.py
@@ -50,3 +50,36 @@ class SkipHandler(web.RequestHandler):
 class SearchHandler(web.RequestHandler):
     def initialize(self, core):
         self.core = core
+
+    def error(self, code, message):
+        self.write({
+            'error': code,
+            'message': message
+        })
+
+        self.set_status(code, message)
+
+    def post(self):
+        field = self.get_body_argument('field','')
+        values = self.get_body_argument('values','')
+
+        if not field:
+            return self.error(400, 'Please provide a field')
+
+        search = {field: [values]}
+
+        search_result = self.core.library.search(search).get()[0]
+
+        pprint( search_result)
+
+        pprint(search_result.artists)
+        self.set_header("Content-Type", "application/json")
+        self.write("""{
+            "uri": "%s",
+            "albums": %s,
+            "artists": %s,
+            "tracks": %s
+        }""" % (search_result.uri,
+                json.dumps(search_result.albums, cls=ModelJSONEncoder),
+                json.dumps(search_result.artists, cls=ModelJSONEncoder),
+                json.dumps(search_result.tracks, cls=ModelJSONEncoder)))
