46f3067650001454ed99351cc5569813a378dcec
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-02-02 00:17:31 +0100

    Delete votes when track is over.

--- mopidy_jukebox/frontend.py
+++ mopidy_jukebox/frontend.py
@@ -2,15 +2,18 @@ import pykka
 
 from mopidy import core
 
+from models import Vote
+
 
 class JukeboxFrontend(pykka.ThreadingActor, core.CoreListener):
     def __init__(self, config, core):
         super(JukeboxFrontend, self).__init__()
         self.core = core
+        core.tracklist.set_consume(True)
 
     def track_playback_ended(self, tl_track, time_position):
         # Remove old votes
-        pass
+        Vote.delete().where(Vote.track_uri == tl_track.track.uri).execute()
 
     def track_playback_started(self, tl_track):
         pass
