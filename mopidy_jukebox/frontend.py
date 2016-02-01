6890623715209d6b39d54f8dbfd1a42baed968c1
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-02-01 01:24:33 +0100

    Frontend stub

--- mopidy_jukebox/frontend.py
+++ mopidy_jukebox/frontend.py
@@ -7,3 +7,10 @@ class JukeboxFrontend(pykka.ThreadingActor, core.CoreListener):
     def __init__(self, config, core):
         super(JukeboxFrontend, self).__init__()
         self.core = core
+
+    def track_playback_ended(self, tl_track, time_position):
+        # Remove old votes
+        pass
+
+    def track_playback_started(self, tl_track):
+        pass
